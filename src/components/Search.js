2317b5fc1573fd5fc4469fb89a6a50b93aa45254
horacio <daddarioh@gmail.com>
2022-08-23 11:59:27 -0300

    added container for image

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,7 +8,9 @@ const Search = () => {
       <div className="Search-text">Search by #</div>
       <div className="Container-input form-group">
         <input className="form-control" />
-        <img src={loupe} alt="Search" />
+        <div className="Container-img">
+          <img src={loupe} alt="Search" />
+        </div>
       </div>
     </Fragment>
   );
