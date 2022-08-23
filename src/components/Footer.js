969bc5278fb26c0ecf57b8fc3d798213e52b4ea5
horacio <daddarioh@gmail.com>
2022-08-23 11:20:55 -0300

    modified to svg image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import logoFooter from "../assets/imgs/logo-footer.png";
+
+import logoFooter from "../assets/imgs/logo-footer.svg";
 
 const Footer = () => {
   return (
@@ -7,14 +8,7 @@ const Footer = () => {
       <div className="Footer-1"></div>
       <div className="Footer-2">
         <div className="d-flex align-items-center justify-content-center h-100">
-          <div className="Logo-footer d-flex align-items-end">
-            <img src={logoFooter}></img>
-            <div className="Gallery-footer">
-              gallery
-              <div className="Rectangle"></div>
-              <div className="Rectangle-2"></div>
-            </div>
-          </div>
+          <img src={logoFooter} className="Logo-footer" alt="Logo" />
           <div className="Text">© All right reserved 2020</div>
         </div>
       </div>
