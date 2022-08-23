60057b7c8f7d8ef9af2249f97e0db360828f6e41
horacio <daddarioh@gmail.com>
2022-08-23 10:53:43 -0300

    logo fixed in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,13 @@
 import React from "react";
 
-import logo from "../assets/imgs/logo.png";
+import logoNav from "../assets/imgs/logo-nav.svg";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
         <a className="navbar-brand" href="#">
-          <div>
-            <img src={logo} alt="Acme" className="d-inline-block align-text-top" />
-            <div className="Gallery">
-              gallery
-              <div>
-                <div className="Rectangle"></div>
-                <div className="Rectangle-2"></div>
-              </div>
-            </div>
-          </div>
+          <img src={logoNav} alt="Logo" />
         </a>
         <button type="button" className="Button-login btn btn-primary">
           LOGIN
